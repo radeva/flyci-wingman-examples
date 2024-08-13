152849e4f7ee7a5ca6beceb74dac7daaeaa8c6ee
radeva <vesita@gmail.com>
2024-08-13 15:13:17 +0300

    Python Memory Error

---
+++ python/11-memory-error.py
@@ -0,0 +1,11 @@
+# main.py
+
+
+def main():
+    large_list = [0] * (
+        10**9
+    )  # Attempting to create a list with more than a billion elements
+
+
+if __name__ == "__main__":
+    main()
